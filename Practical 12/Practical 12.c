a35d9e065b18e9e78882187bb05cdeb7e1652b83
Yatridekivadiya <yatridekivadiya1012@gmail.com>
2025-08-18 15:48:06 +0530

    Add files via upload

---
+++ Practical 12/Practical 12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int id;
+    for
+        (id=1;id<=50;id++)
+        {
+            printf("\nBook id: %d",id);
+            if(id%5==0)
+            {
+                printf("(Special Edition)");
+            }
+        }
+        printf("\nthis program is developed by yatri dekivadiya");
+        return 0;
+}
