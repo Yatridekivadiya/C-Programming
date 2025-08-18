864bc56b75ac722904a32d63ebfcf9745cdeaf2a
Yatridekivadiya <yatridekivadiya1012@gmail.com>
2025-08-18 16:14:33 +0530

    Add files via upload

---
+++ Practical 13/Practical 13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    for(i=0;i<=100;i=i+10)
+    {
+        printf("\ncurrent_water_level : %d",i);
+    }
+    printf("\ntank is full");
+    printf("\nthis program is developed by yatri dekivadiya");
+}
