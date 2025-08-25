dbd37201bca1c45e712f46736543da3c6411a440
Yatridekivadiya <yatridekivadiya1012@gmail.com>
2025-08-25 15:17:53 +0530

    Add files via upload

---
+++ practical 18/practical 18.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int rows,cols,i,j;
+    printf("Enter the size of table vertically :");
+    scanf("%d",&rows);
+    printf("Enter the size of table horizontally :");
+    scanf("%d",&cols);
+    printf("Multiplication Table (%d x %d) :\n",rows,cols);
+    if(rows,cols<=0)
+    {
+        printf("inmvalid input");
+    }
+    for(i=1;i<=rows;i++)
+    {
+        for(j=1;j<=cols;j++)
+        {
+            printf("\t%d",i*j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
